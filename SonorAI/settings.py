e8c3dd84831f2fb013e5e91c6b6e232704406705
NA-Silk <nicolasseda@gmail.com>
2025-11-25 22:18:52 -0500

    Prepared web deployment

--- SonorAI/settings.py
+++ SonorAI/settings.py
@@ -11,22 +11,32 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
+
+# Use environ to extract environment variables
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Get environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5j&fydjjmcs9%a)-$fckmh^2ku_37n5n$3t_+kwad9apx1@+#4"
+#SECRET_KEY = "django-insecure-5j&fydjjmcs9%a)-$fckmh^2ku_37n5n$3t_+kwad9apx1@+#4"
+SECRET_KEY = env("SECRET_KEY", default="change_me") # pyright: ignore[reportArgumentType]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", default=False) # pyright: ignore[reportArgumentType]
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", default=["*"]) # pyright: ignore[reportArgumentType]
 
 
 # Application definition
@@ -43,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", # whitenoise
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -60,6 +71,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -74,11 +86,14 @@ WSGI_APPLICATION = "SonorAI.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db(default="sqlite:///db.sqlite3"), # pyright: ignore[reportArgumentType]
 }
 
 
@@ -100,6 +115,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+LOGGING = {  
+    "version": 1,  
+    "disable_existing_loggers": False,  
+    "handlers": {"console": {"class": "logging.StreamHandler"}},  
+    "loggers": {"": {"handlers": ["console"], "level": "DEBUG"}},  
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
@@ -116,12 +139,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+#STATIC_URL = "static/"
+STATIC_URL = env.str("STATIC_URL", default="/static/")   # pyright: ignore[reportArgumentType]
+STATIC_ROOT = env.str("STATIC_ROOT", default=BASE_DIR / "staticfiles") # pyright: ignore[reportArgumentType]
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,"SonorAI/static/"),
 )
+
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = DEBUG
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Media files
+
+MEDIA_ROOT = env("MEDIA_ROOT", default=BASE_DIR / "media") # pyright: ignore[reportArgumentType]
+MEDIA_URL = env("MEDIA_PATH", default="/media/") # pyright: ignore[reportArgumentType]
